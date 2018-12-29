559f17a888e92828dd123eb179dd40b2b53bbff6
nimtego <nimtego@gmail.com>
2018-12-29 10:59:59 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/spring_kotlin_sample/SpringKotlinSampleApplication.kt
@@ -0,0 +1,12 @@
+package com.example.spring_kotlin_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinSampleApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/example/spring_kotlin_sample/SpringKotlinSampleApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.example.spring_kotlin_sample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringKotlinSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
